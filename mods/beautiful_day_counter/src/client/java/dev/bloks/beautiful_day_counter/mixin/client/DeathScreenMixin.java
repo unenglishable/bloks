8ba4e9be104bacf45a3c7c37437119d92b93c252
unenglishable <unenglishable@gmail.com>
2026-02-02 15:05:59 -1000

    fix(beautiful_day_counter): allow death score text decoration
    Shadow DeathScreen.scoreText as @Mutable so we can temporarily append the day summary; without it, the JVM threw IllegalAccessError when the mixin tried to assign to the final field during render.

--- mods/beautiful_day_counter/src/client/java/dev/bloks/beautiful_day_counter/mixin/client/DeathScreenMixin.java
+++ mods/beautiful_day_counter/src/client/java/dev/bloks/beautiful_day_counter/mixin/client/DeathScreenMixin.java
@@ -7,7 +7,9 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.DeathScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DeathScreen.class)
 public abstract class DeathScreenMixin extends Screen {
-  @Shadow private Text scoreText;
+  @Shadow @Final @Mutable private Text scoreText;
 
   @Unique private Text beautiful_day_counter$baseScoreText;
 
