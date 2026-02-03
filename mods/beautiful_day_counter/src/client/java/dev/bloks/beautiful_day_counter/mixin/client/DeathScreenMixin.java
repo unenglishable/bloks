2a807a449d70ee82d8f2f16d65a06a455ee70847
unenglishable <unenglishable@gmail.com>
2026-02-02 18:47:49 -1000

    chore(beautiful_day_counter): reformat after spotless
    Fix spotless violations in ConfigScreen and DeathScreenMixin so CI passes.

--- mods/beautiful_day_counter/src/client/java/dev/bloks/beautiful_day_counter/mixin/client/DeathScreenMixin.java
+++ mods/beautiful_day_counter/src/client/java/dev/bloks/beautiful_day_counter/mixin/client/DeathScreenMixin.java
@@ -23,7 +23,8 @@ public abstract class DeathScreenMixin extends Screen {
   @Shadow @Final @Mutable private Text scoreText;
 
   @Unique private Text beautiful_day_counter$baseScoreText;
-  @Unique private static final SoundEvent BEAUTIFUL_DAY_COUNTER$HIKARI_CLIP =
+  @Unique
+  private static final SoundEvent BEAUTIFUL_DAY_COUNTER$HIKARI_CLIP =
       SoundEvent.of(Identifier.of("beautiful_day_counter", "hikari_8_bit"));
   @Unique private boolean beautiful_day_counter$clipPlayed;
 
